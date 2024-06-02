00d49149b3aed3d9ce57aea14b9baeaf7d8c12c9
ilya2.0 <iprez4031@gmail.com>
2024-06-02 13:09:07 +0300

    feature_InvokedListner

--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -10,32 +10,6 @@ public class User {
     private String password;
     private String email;
 
-    public User() {
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     @Override
     public boolean equals(Object o) {
