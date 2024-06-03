a8e83bb1a5d224491875bf9f736c11eaec99949e
iihnatovich <i.ihnatovich@gmail.com>
2024-06-03 13:09:42 +0300

    parallel API impl

--- src/test/java/baseEntities/BaseApiTest.java
+++ src/test/java/baseEntities/BaseApiTest.java
@@ -1,10 +1,11 @@
 package baseEntities;
 
+import com.google.gson.Gson;
 import configuration.ReadProperties;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.protocol.HTTP;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeClass;
 import services.ProjectService;
 import services.TestCaseService;
 
@@ -14,9 +15,13 @@ public class BaseApiTest {
 
     protected TestCaseService testCaseService;
     protected ProjectService projectService;
+    protected Gson gson;
 
-    @BeforeTest
+
+    @BeforeClass
     public void setupApi() {
+        gson = new Gson();
+
         testCaseService = new TestCaseService();
         projectService = new ProjectService();
         RestAssured.baseURI = ReadProperties.getBaseApiUrl();
