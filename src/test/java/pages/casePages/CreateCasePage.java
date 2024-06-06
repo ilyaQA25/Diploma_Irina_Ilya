ef85b2aae4c304711195b113e357cd75ba98f7ec
iihnatovich <i.ihnatovich@gmail.com>
2024-06-06 16:12:28 +0300

    added negative tests with Hint elem

--- src/test/java/pages/casePages/CreateCasePage.java
+++ src/test/java/pages/casePages/CreateCasePage.java
@@ -4,13 +4,15 @@ import baseEntities.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import wrappers.Button;
+import wrappers.Hint;
 import wrappers.Input;
 
-public class CreateCasePage extends BasePage {  // move TO AllCasePage ???????
+public class CreateCasePage extends BasePage {
 
     private By modalCreateCaseLocator = By.xpath("//div[@class = 'modal-content']");
     private By testCaseTitleLocator = By.xpath("//input[@data-testid='textbox-prop-title']");
     private By createCaseButtonLocator = By.xpath("//button[@data-testid='button-save-entity']");
+    private By hintTitleErrorLocator = By.xpath("//div[@data-testid='ValInput_ErrorContainer']");
     public CreateCasePage(WebDriver driver) {
         super(driver);
     }
@@ -28,6 +30,10 @@ public class CreateCasePage extends BasePage {  // move TO AllCasePage ???????
         return new Button(driver, createCaseButtonLocator);
     }
 
+    public Hint getHintTitleError() {
+        return new Hint(driver, hintTitleErrorLocator);
+    }
+
     public void enterCaseTitle(String caseName) { // void or this.CreateCasePage??????
         getCaseTitleInput().sendKeys(caseName);
     }
@@ -43,5 +49,13 @@ public class CreateCasePage extends BasePage {  // move TO AllCasePage ???????
 
     public boolean isCreateButtonEnabled() {
         return getCreateCaseButton().isEnabled();
+    }
+
+    public boolean isHintTitleErrorDisplayed() {
+        return getHintTitleError().isDisplayed();
+    }
+
+    public String getHintTitleErrorText() {
+        return getHintTitleError().getText();
     }
 }
---
+++ src/test/java/tests/gui/HintNegativeTests.java
@@ -0,0 +1,38 @@
+package tests.gui;
+
+import baseEntities.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import steps.CreatingTcSteps;
+import steps.NavigationSteps;
+
+public class HintNegativeTests extends BaseTest {
+    private NavigationSteps navigationSteps;
+    private CreatingTcSteps creatingTcSteps;
+
+    @BeforeMethod
+    public void initSteps() {
+        navigationSteps = new NavigationSteps(driver);
+        creatingTcSteps = new CreatingTcSteps(driver);
+    }
+
+    @Test
+    public void incorrectDataInputCaseTest() {
+        navigationSteps.navigateAllCasesPage();
+        creatingTcSteps.createNewCase();
+        creatingTcSteps.enterCaseTitle("     ");
+
+        Assert.assertEquals(creatingTcSteps.getHintTitleErrorText(), "Must have at least 1 characters " +
+                "(leading/trailing white spaces not counted).");
+    }
+
+    @Test
+    public void failedOnPurposeTest() {
+        navigationSteps.navigateAllCasesPage();
+        creatingTcSteps.createNewCase();
+        creatingTcSteps.enterCaseTitle("     ");
+
+        Assert.assertEquals(creatingTcSteps.getHintTitleErrorText(), "Must have at least 1 characters");
+    }
+}
